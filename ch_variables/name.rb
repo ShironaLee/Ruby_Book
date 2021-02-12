49bb22ec0da53557070c60b2ac0a043e3200d30a
Amanda S. Lee <mandaslee@gmail.com>
2021-02-12 03:46:43 +0000

    Completed exercises in second chapter of Intro to Ruby

---
+++ ch_variables/name.rb
@@ -0,0 +1,9 @@
+puts "What is your first name? Type it below!"
+first_name = gets.chomp 
+
+puts "Great, now what is your last name?"
+last_name = gets.chomp
+
+puts "Nice to meet you, #{first_name} #{last_name}!"
+
+
---
+++ ch_variables/variables_5.rb
@@ -0,0 +1,2 @@
+# the first example prints 3 for a value of x
+# the second variable prints an error due to x being initalized inside a block, but not being available within the outer scope.  
